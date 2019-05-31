d522e2c2ab5bb0bcc73ebece75cf0b309f6c108a
Darina <darinakuleva@gmail.com>
2019-05-31 17:31:27 +0300

    TASK-3 Рефакторинг TaskInformation

--- src/components/TaskPage/TaskInformation.jsx
+++ src/components/TaskPage/TaskInformation.jsx
@@ -7,69 +7,51 @@ import PropTypes from 'prop-types'
 class TaskInformation extends React.PureComponent {
 
   static propTypes = {
-    todoList: PropTypes.array.isRequired
-  };
+    todoList: PropTypes.array.isRequired,
+  }
 
   render() {
     const {
       todoList,
       match: {
         params: {
-          id
-        }
+          id,
+        },
       },
     } = this.props
 
-    const openTask = todoList.filter(todoItem => {
+    const [openTask] = todoList.filter( todoItem => {
       return todoItem.id === +id
-    })
-
-    const createdDate = openTask.map(date =>
-      <div key={date.id}>
-        {date.createdAtDate.getDate()}.{date.createdAtDate.getMonth() + 1}
-        <NBSP/>
-        at
-        <NBSP/>
-        {date.createdAtDate.getHours()}
-        <NBSP/>
-        o'clock
-        <NBSP/>
-        {date.createdAtDate.getMinutes()}
-        <NBSP/>
-        minutes
-      </div>,
-    )
-
-    const doneTime = openTask.map(date =>
-      <div key={date.id}>
-        {date.done
-          ? `task done ${ date.doneAtDate.getDate() }.${ (date.createdAtDate.getMonth() + 1) } 
-            at ${ date.doneAtDate.getHours() } o'clock ${ date.doneAtDate.getMinutes() } minutes`
-          : 'task not completed'}
-      </div>,
-    )
+    } )
 
     return (
       <>
         <h2>Task Information</h2>
         <div>
-          {
-            openTask.map(todoItem =>
-              <div key={todoItem.id}>
-                <div>
-                  {todoItem.text}
-                </div>
-                <div>
-                  task created
-                  <NBSP/>
-                  {createdDate}
-                </div>
-                <div>
-                  {doneTime}
-                </div>
-              </div>
-            )
-          }
+          <div>
+            { openTask.text }
+          </div>
+          <div>
+            task created
+            <NBSP/>
+            { openTask.createdAtDate.getDate() }.{ openTask.createdAtDate.getMonth() + 1 }
+            <NBSP/>
+            at
+            <NBSP/>
+            { openTask.createdAtDate.getHours() }
+            <NBSP/>
+            o'clock
+            <NBSP/>
+            { openTask.createdAtDate.getMinutes() }
+            <NBSP/>
+            minutes
+          </div>
+          <div>
+            { openTask.done
+              ? `task done ${ openTask.doneAtDate.getDate() }.${ (openTask.createdAtDate.getMonth() + 1) } 
+            at ${ openTask.doneAtDate.getHours() } o'clock ${ openTask.doneAtDate.getMinutes() } minutes`
+              : 'task not completed' }
+          </div>
         </div>
         <Link to="/todo">Todo List</Link>
       </>
@@ -77,10 +59,10 @@ class TaskInformation extends React.PureComponent {
   }
 }
 
-function mapStateToProps(state) {
+function mapStateToProps( state ) {
   return {
-    todoList: state.todoList
+    todoList: state.todoList,
   }
 }
 
-export default connect(mapStateToProps)(TaskInformation)
+export default connect( mapStateToProps )( TaskInformation )
